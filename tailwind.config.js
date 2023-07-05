611dc4698420d9ae2b66f90b3b394da074ee9a45
Ruth James <jamesruth449@gmail.com>
2023-07-05 20:46:22 +0100

    mobile complete

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,9 @@ module.exports = {
       'dark-cyan': 'hsl(185, 75%, 39%)',
       'very-dark-desaturated-blue': 'hsl(229, 23%, 23%)',
       'dark-grayish-blue': 'hsl(227, 10%, 46%)',
-      'dark-gray': 'hsl(0, 0%, 59%)'
+      'dark-gray': 'hsl(0, 0%, 59%)',
+      'gray': 'hsla(228, 9%, 92%, 1)',
+      'white': 'hsla(0, 0%, 100%, 1)'
     },
     fontFamily: {
       KumbhSans: ['Kumbh Sans', 'cursive']
