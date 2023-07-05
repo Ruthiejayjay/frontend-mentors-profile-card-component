0b99071c7fc8cf3d45e187ff89820e4d3207bf7b
Ruth James <jamesruth449@gmail.com>
2023-07-05 19:17:05 +0100

    setup complete

---
+++ src/components/CardComponent.vue
@@ -0,0 +1,6 @@
+<script></script>
+<template>
+    <div class="w-full h-[667px] bg-dark-cyan flex flex-col">
+       <div class="bg-[url]"></div>
+    </div>
+</template>
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,15 @@ module.exports = {
   content: ['./public/**/*.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
     extend: {},
+    colors: {
+      'dark-cyan': 'hsl(185, 75%, 39%)',
+      'very-dark-desaturated-blue': 'hsl(229, 23%, 23%)',
+      'dark-grayish-blue': 'hsl(227, 10%, 46%)',
+      'dark-gray': 'hsl(0, 0%, 59%)'
+    },
+    fontFamily: {
+      KumbhSans: ['Kumbh Sans', 'cursive']
+    }
   },
   plugins: [],
 }
