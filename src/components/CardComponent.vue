25b8f74584aba402bd1e9088dc401a33fe4ef3d7
Ruth James <jamesruth449@gmail.com>
2023-07-06 20:17:39 +0100

    desktop complete

--- src/components/CardComponent.vue
+++ src/components/CardComponent.vue
@@ -4,10 +4,10 @@ import StatsItem from './StatsItem.vue';
 <template>
     <div class="w-full min-h-[667px] h-full bg-dark-cyan flex flex-col">
         <div
-            class="bg-[url('../assets/images/bg-pattern-top.svg')] bg-no-repeat bg-cover h-[250px] -rotate-[-135deg] -ml-80">
+            class="bg-[url('../assets/images/bg-pattern-top.svg')] bg-no-repeat bg-cover h-[250px] -rotate-[-135deg] -ml-80 md:h-[550px] md:-ml-[1080px]">
         </div>
-        <div class=" -mt-36 w-[326px] h-[374px] bg-white mx-6 rounded-2xl z-30 flex flex-col">
-            <div class="w-[326px] h-[140px] rounded-t-2xl">
+        <div class=" -mt-36 w-[326px] h-[374px] bg-white mx-6 rounded-2xl z-30 flex flex-col md:-mt-96 md:mx-96 md:w-[350px]">
+            <div class="w-[326px] h-[140px] rounded-t-2xl md:w-[350px]">
                 <img src="../assets/images/bg-pattern-card.svg" alt="bg-pattern-card" class=" rounded-t-2xl">
             </div>
             <div class="grid place-items-center">
@@ -39,6 +39,6 @@ import StatsItem from './StatsItem.vue';
                 </div>
             </div>
         </div>
-        <div class="bg-[url('../assets/images/bg-pattern-bottom.svg')] bg-no-repeat bg-cover h-[250px] -rotate-12 -mr-60"></div>
+        <div class="bg-[url('../assets/images/bg-pattern-bottom.svg')] bg-no-repeat bg-cover h-[250px] -rotate-[90deg] -mr-60 md:h-[550px] md:-mr-0 md:-rotate-[20deg]"></div>
     </div>
 </template>
